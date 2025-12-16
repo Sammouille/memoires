6a39e439c5484eb5cee1235c15419f7a1ff94af6
Sam <samuel.wolf@hotmail.fr>
2025-12-16 15:36:17 +0100

    on peut intergair avec la machinie

--- interactible_machine_txt.gd
+++ interactible_machine_txt.gd
@@ -3,4 +3,8 @@ extends Interactible
 @export var machine_txt : Control
 
 func interaction():
-	machine_txt.input_node.grab_focus()
+	$Control.show()
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		$Control.hide()
