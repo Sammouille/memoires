b5ccb5514e4154489ea0bd931612a72b5c7442f6
kingBrie <b.remy1374@gmail.com>
2025-12-16 10:24:44 +0100

    toute la partie déplacement 3D faite + caméra + PNJ qui est tjrs face caméra

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -2,7 +2,26 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 3.5
+
+#BOB head
+@export var Bob_freq = 2.0
+@export var bob_amp = 0.08
+@export var t_bob = 0.0
+
+@export var sensitivity = 0.01
+
+@onready var headPOV = $head/headVIEW
+@onready var head = $head
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * sensitivity)
+		headPOV.rotate_x(-event.relative.y * sensitivity)
+		headPOV.rotation.x = clamp(headPOV.rotation.x, deg_to_rad(-60), deg_to_rad(130))
 
 
 func _physics_process(delta: float) -> void:
@@ -17,7 +36,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -25,4 +44,14 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
+	#head bob
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	headPOV.transform.origin = _headbob(t_bob)
+
 	move_and_slide()
+
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * Bob_freq) * bob_amp
+	pos.x = cos(time * Bob_freq /2) * bob_amp
+	return pos
