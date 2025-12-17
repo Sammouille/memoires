a305c170fbd4ed1e3cdc5f2547b356ba71f8c714
Sam <samuel.wolf@hotmail.fr>
2025-12-17 15:22:41 +0100

    Les livres

--- code/livreInteractible.gd
+++ code/livreInteractible.gd
@@ -4,14 +4,16 @@ extends Interactible
 @export var voix : AudioStream
 @export var nom: String = "manuel"
 
+@export var interface_livre : Control
+
 func debutInteraction():
-	%InterfaceLivre.cleanBoiteDialogue()
-	%InterfaceLivre.initxt(accroche, nom)
-	%InterfaceLivre.inivoix(voix)
-	%InterfaceLivre.show()
+	interface_livre.cleanBoiteDialogue()
+	interface_livre.initxt(accroche, nom)
+	interface_livre.inivoix(voix)
+	interface_livre.show()
 	
 
 func finInteraction():
-	%InterfaceLivre.hide()
-	%InterfaceLivre.cleanBoiteDialogue()
+	interface_livre.hide()
+	interface_livre.cleanBoiteDialogue()
 	
